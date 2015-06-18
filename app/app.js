e88afe96ccac602f94287611e065edc8f3fc0826
Antony Bello <antony.bello@pomona.edu>
2015-06-18 13:46:35 -0700

    Added GMS API service

--- app/app.js
+++ app/app.js
@@ -1,4 +1,11 @@
 var application = require("application");
+
+application.onLaunch = function() {
+    console.log('Providing GoogleMap API key...');
+    // NOTE: Use the Google documentation to obtain API key: https://developers.google.com/maps/documentation/ios/start
+    GMSServices.provideAPIKey(""); // API KEY HERE!
+}
+
 application.mainModule = "main-page";
 application.cssFile = "./app.css";
 application.start();
